2cc3e7ab23b9ffbf3dab94ddf1a20f4386c8e292
kogoto-ops <mosetikogoto@gmail.com>
2025-10-05 20:07:34 +0000

    Added files to ann's file , OPS

---
+++ age.py
@@ -0,0 +1,10 @@
+age = int(input("Введите ваш возраст: "))
+
+if age >= 18:
+    print("Вы совершеннолетний.")
+else:
+    print("Вы несовершеннолетний.")
+
+is_member = age >= 65
+print(f"Пенсионер: {is_member}")
+print("Pensioners are usually above 65 years and retired")
---
+++ massices.py
@@ -0,0 +1,23 @@
+fruits = ["apple", "banana", "cherry"]
+fruits.append("date")
+
+coordinates = (10, 20)
+
+unique_nums = {1, 2, 2, 3, 1}
+
+person = {"name": "Alice", "age": 28, "is_student": False}
+
+print("list:", fruits)
+print("Coordinates:", coordinates)
+print("Unique numbers:", unique_nums)
+print("Dictionary person:", person)
+
+#print types
+
+print(type(fruits))
+print(type(coordinates))
+
+print(type(unique_nums))
+
+print(type(person))
+
